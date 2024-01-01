1d8c2266a3b480459ca4a1ebd5f8b19546cc28cf
Александр Стародубов <alexstar.97@mail.ru>
2024-01-02 00:32:32 +0300

    final

--- Vk_Photo_Downloader/Vk_Photo_Downloader.cpp
+++ Vk_Photo_Downloader/Vk_Photo_Downloader.cpp
@@ -13,10 +13,10 @@
 namespace fs = boost::filesystem;
 using namespace std;
 
-HRESULT DownloadFile(string line, fs::path current_dir, fs::path input_dir, string chat_name, string& photo_full_name, int& k) {
+HRESULT DownloadFile(string line, fs::path current_dir, fs::path input_dir, string chat_name, string& photo_full_name, int& k) 
+{
     int start = line.find("https://sun");
     int end = line.find("=album") + 6; 
-
     bool find_standart_dir = false;
 
     string link = line.substr(start, end - start);
@@ -68,39 +68,34 @@ HRESULT DownloadFile(string line, fs::path current_dir, fs::path input_dir, stri
     return download_start;
 }
 
-bool ReadFileHtml(fs::path current_dir, fs::path input_dir, int& k)
+bool ReadFileHtml(fs::path current_dir, fs::path input_dir, int& counter)
 {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
     string line;
     string chat_name = " "; //default name
-    string photo_full_name = " "; // для вывода ниже надо . надо ли 
+    string photo_full_name = " "; 
     ifstream in(current_dir.string());
-    int s;
 
     if (in.is_open())
     {
         while (getline(in, line))
         {
-            if (line.find("<div class=\"ui_crumb\" >") != std::string::npos) // название чата всегда раньше самой переписки и фоток
+            if (line.find("<div class=\"ui_crumb\" >") != std::string::npos) // the name of the chat is always before the correspondence and photos themselves
             {
-
-                int start_name = line.rfind("\" >") + 3; // начало имени
-                int end_name = line.find("</div>", start_name - 1); // конец имени. Ищем div начиная с начала имени.
-
+                int start_name = line.rfind("\" >") + 3; // the beginning of the name
+                int end_name = line.find("</div>", start_name - 1); // end of the name. We are looking for a div starting from the beginning of the name.
                 chat_name = line.substr(start_name, end_name - start_name);
             }
-
             if (line.find("jpg") != std::string::npos)
             {
                 if (line.find("https://sun") != std::string::npos)
                 {
-                    if (DownloadFile(line, current_dir, input_dir, chat_name, photo_full_name, k) == S_OK)
+                    if (DownloadFile(line, current_dir, input_dir, chat_name, photo_full_name, counter) == S_OK)
                     {
                         SetConsoleTextAttribute(hConsole, 10);
                         cout << "Photo number ";
                         SetConsoleTextAttribute(hConsole, 12);
-                        cout << k++;
+                        cout << counter++;
                         SetConsoleTextAttribute(hConsole, 10);
                         cout << " is being downloaded from: ";
                         SetConsoleTextAttribute(hConsole, 11);
@@ -110,7 +105,7 @@ bool ReadFileHtml(fs::path current_dir, fs::path input_dir, int& k)
                         SetConsoleTextAttribute(hConsole, 14);
                         cout << photo_full_name<<endl;
                         SetConsoleTextAttribute(hConsole, 15);
-                        k++;
+                       
                     }
                 }
             }
@@ -121,7 +116,6 @@ bool ReadFileHtml(fs::path current_dir, fs::path input_dir, int& k)
     in.close();
     return 0;
 }
-
 
 bool CreateDirectories(fs::path folder)
 {
@@ -137,7 +131,6 @@ bool CreateDirectories(fs::path folder)
                                "\\OTHER"
                              };
 
-   
     bool root_folder_exist = false;
 
     folder += final_folder[0]; 
@@ -173,38 +166,35 @@ bool CreateDirectories(fs::path folder)
         }
 
    }
-    
     Sleep(1000);
-
     return 1;
 }
 int main()
 {
-   
     setlocale(LC_ALL, "RUS");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    HWND hwnd = GetConsoleWindow(); // Узнаём сразу hwnd окна    
-    MoveWindow(hwnd, 300, 200, 1300, 700, TRUE);//xnew,ynew,wnew,hnew -новые положение x,y, ширина и высота
-
-    int k = 0;
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // for change console color
-   //system("mode con cols=180 lines=50"); + чтобы можно было вверх вниз 
+    HWND hwnd = GetConsoleWindow(); // getting the window descriptor (handle)
+    MoveWindow(hwnd, 300, 200, 1300, 700, TRUE); // changing the window size
+    
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // getting a console descriptor (handle) to change the text color
    
-    //fs::path p("C:\\Users\\Alex\\Downloads\\Archive"); // стандартный путь к папке
     fs::path input_dir("C:\\Users\\Alex\\Downloads\\Archive");
     cout << "Enter the full path to the folder. "
         "Example: \"C:\\Users\\Alex\\Downloads\\Archive\" \n";
-    
+
+    int counter = 0;
     string input;
+    SetConsoleTextAttribute(hConsole, 11);
     getline(cin, input);
+    SetConsoleTextAttribute(hConsole, 15);
     input_dir = input;
     cout <<"The entered directory: "<< input_dir << endl;
   
     try
     {
-        CreateDirectories(input_dir); //p
+        CreateDirectories(input_dir); 
     }
     catch (const fs::path& problematic_folder) // to save memory, we send it by link
     {
@@ -214,7 +204,7 @@ int main()
         SetConsoleTextAttribute(hConsole, 15);
         return 0;
     }
-    catch (const exception& e)
+    catch (const exception& e) // catch all exceptions
     {
         SetConsoleTextAttribute(hConsole, 12);
         cerr << e.what();
@@ -223,18 +213,16 @@ int main()
     }
 
     fs::path current_dir;
-    for (fs::recursive_directory_iterator i(input_dir), end; i != end; i++) // проход по всем файлам и папкам
+    for (fs::recursive_directory_iterator i(input_dir), end; i != end; i++) // going through all the files
     {
-        if (!is_directory(i->path())) // если это файл
+        if (!is_directory(i->path())) // if it 's not a directory , but a file
         {
-            current_dir = i->path(); // ТУТ МЕНЯЕТ ИСХОДНЫЙ ПУТЬ
-            //cout << "----------------"<<input_dir<<"++++++++++++++++++++";
+            current_dir = i->path(); 
             if (fs::path(current_dir.filename()).extension() == ".html")
             {
-                ReadFileHtml(current_dir, input_dir, k);  
+                ReadFileHtml(current_dir, input_dir, counter);  
             }
-
         }
     }
-    cout << "Total photos found: " << k;
+    cout << "Total photos found: " << counter;
 }
